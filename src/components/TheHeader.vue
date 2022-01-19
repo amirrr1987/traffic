7cc3b9dfa4841df399d54c7cef866c04a9577a2d
Amirrr1987 <Maghami.A1987@gmail.com>
2022-01-19 20:13:02 +0330

    feat: traffic light
    - create trafficLight component

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,6 +1,11 @@
 <template>
-  <header class="header bg-center bg-cover h-[50vw]"></header>
+  <header class="header bg-center bg-cover h-[50vw]">
+    <TrafficLight/>
+  </header>
 </template>
+<script setup lang="ts">
+import TrafficLight from './TrafficLight.vue';
+</script>
 <style>
 .header {
   background-image: url("./../assets/img/bg.svg");
---
+++ src/components/TrafficLight.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <img src="./../assets/img/traffic-light.svg" title="" alt="" />
+    <img src="./../assets/img/green.svg" title="" alt="" />
+    <img src="./../assets/img/yellow.svg" title="" alt="" />
+    <img src="./../assets/img/red.svg" title="" alt="" />
+  </div>
+</template>
