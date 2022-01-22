89758877af345ea868276086bf3fe435cc576f53
Amirrr1987 <Maghami.A1987@gmail.com>
2022-01-23 00:46:01 +0330

    fix: animation time
    - sort TrafficLight order
    - beautify codes
    - set time for red yellow green
    - install VueUse package

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -3,18 +3,18 @@
     class="header bg-center bg-cover relative overflow-hidden w-full aspect-[16/7.5]"
   >
     <TrafficLight
-      :redStatus="true"
-      :yellowStatus="true"
-      :greenStatus="true"
+      :redStatus="redStatus"
+      :yellowStatus="yellowStatus"
+      :greenStatus="greenStatus"
       leftPosition="30.5vw"
       topPosition="3.8vw"
       heightSize="22vw"
       zIndex="0"
     />
     <TrafficLight
-      :redStatus="true"
-      :yellowStatus="true"
-      :greenStatus="true"
+      :redStatus="redStatus"
+      :yellowStatus="yellowStatus"
+      :greenStatus="greenStatus"
       leftPosition="71vw"
       topPosition="6vw"
       heightSize="28vw"
@@ -25,21 +25,20 @@
       topPosition="20vw"
       heightSize="11vw"
       zIndex="4"
-      class="left-right"
     />
     <TheOrangeCar
       leftPosition="54vw"
       topPosition="30vw"
       heightSize="18vw"
       zIndex="7"
-      class="scale-out-top"
     />
     <TheOldMan
       leftPosition="19vw"
       topPosition="23vw"
       heightSize="14vw"
       zIndex="6"
-      class="left-rightf"
+      class="old-man-anim"
+   
     />
     <ThePoliceman
       leftPosition="46.2vw"
@@ -59,7 +58,8 @@
       topPosition="19vw"
       heightSize="10vw"
       zIndex="2"
-      class="right-left"
+  
+  
     />
   </header>
 </template>
@@ -71,13 +71,35 @@ import TheOldMan from "./TheOldMan.vue";
 import ThePoliceman from "./ThePolice.vue";
 import TheVan from "./TheVan.vue";
 import TheGreenCar from "./TheGreenCar.vue";
+
+
+import { onMounted, ref } from "vue";
+
+const redStatus = ref(false)
+const yellowStatus = ref(false)
+const greenStatus = ref(true)
+
+onMounted(() => {
+setTimeout(()=>{
+greenStatus.value = false
+yellowStatus.value = true
+},8000)
+setTimeout(()=>{
+yellowStatus.value = false
+redStatus.value = true
+},9000)
+})
+
+
+
 </script>
 <style>
 .header {
   background-image: url("./../assets/img/bg.svg");
 }
+
 .left-right {
-  animation: left-keyframes 16s linear infinite both;
+  animation: left-keyframes 30s linear infinite both;
 }
 @keyframes left-keyframes {
   0% {
@@ -124,6 +146,26 @@ import TheGreenCar from "./TheGreenCar.vue";
     left: 44vw;
     opacity: 0;
     z-index: 0;
+  }
+}
+.old-man-anim{
+  animation: old-man-keyframes 30s linear infinite both;
+}
+@keyframes old-man-keyframes {
+  0% {
+    left: -20vw;
+  }
+  20% {
+    left: 20vw;
+  }
+  30% {
+    left: 20vw;
+  }
+  70% {
+    left: 100vw;
+  }
+  100% {
+    left: 100vw;
   }
 }
 </style>
